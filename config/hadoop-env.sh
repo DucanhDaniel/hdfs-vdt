f651b8bc9193e810078ff2e47cd3e6dbd2427b1e
Phạm Đức Anh <ducanh2002add@gmail.com>
2026-05-21 15:37:37 +0700

    init

---
+++ config/hadoop-env.sh
@@ -0,0 +1,5 @@
+export JAVA_HOME=$(dirname $(dirname $(readlink -f $(which java))))
+export HDFS_NAMENODE_USER=root
+export HDFS_DATANODE_USER=root
+export HDFS_JOURNALNODE_USER=root
+export HDFS_ZKFC_USER=root
